77ed5db459b190afa56080debe5f4f95fe28902e
kaveetha <kaveetharandili@gmail.com>
2025-06-07 11:41:39 +0530

    additional has been completed

--- app/src/main/java/com/example/my_calculator_app/MainActivity.java
+++ app/src/main/java/com/example/my_calculator_app/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.my_calculator_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +13,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView answer;
+    EditText num1,num2;
+    Button sum, gomul;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        answer = (TextView) findViewById(R.id.add_view);
+        num1 = (EditText) findViewById(R.id.num_01);
+        num2 = (EditText) findViewById(R.id.num_02);
+        sum = (Button) findViewById(R.id.button_sum);
+        gomul = (Button) findViewById(R.id.go_mul);
+
+        sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num_one = Integer.parseInt(num1.getText().toString());
+                int num_two = Integer.parseInt(num2.getText().toString());
+                int ans = num_one+num_two;
+                answer.setText(String.valueOf(ans));
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
