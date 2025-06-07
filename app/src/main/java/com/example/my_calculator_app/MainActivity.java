62add10454b80e5937c2dfe3d81c0d3d8d5854f9
kaveetha <kaveetharandili@gmail.com>
2025-06-07 13:38:37 +0530

    intent added - gomull button

--- app/src/main/java/com/example/my_calculator_app/MainActivity.java
+++ app/src/main/java/com/example/my_calculator_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.my_calculator_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
                 int num_two = Integer.parseInt(num2.getText().toString());
                 int ans = num_one+num_two;
                 answer.setText(String.valueOf(ans));
+            }
+        });
+        gomull.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent();
             }
         });
 
