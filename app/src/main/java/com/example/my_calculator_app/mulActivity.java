1e658c409318d0831742d75ee7f1f4a6ea459eac
kaveetha <kaveetharandili@gmail.com>
2025-06-07 15:33:00 +0530

    Multiplication done with intent mover

--- app/src/main/java/com/example/my_calculator_app/mulActivity.java
+++ app/src/main/java/com/example/my_calculator_app/mulActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,22 +13,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-      Button btn_next;
+public class mulActivity extends AppCompatActivity {
+    TextView answer;
+    EditText num1,num2;
+    Button mul;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mul);
+        answer = (TextView) findViewById(R.id.mul_view);
+        num1 = (EditText) findViewById(R.id.num_01);
+        num2 = (EditText) findViewById(R.id.num_02);
+        mul = (Button) findViewById(R.id.button_mul);
 
-        btn_next = (Button) findViewById(R.id.button_next);
-        btn_next.setOnClickListener(new View.OnClickListener() {
+
+        mul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,mulActivity.class);
-                startActivity(intent);
+                int num_one = Integer.parseInt(num1.getText().toString());
+                int num_two = Integer.parseInt(num2.getText().toString());
+                int ans = num_one * num_two;
+                answer.setText(String.valueOf(ans));
             }
         });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
